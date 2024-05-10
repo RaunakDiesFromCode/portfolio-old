02818d04c1e79128e831521ac9db80530797c669
captainTigerYT <raunakmanna43@gmail.com>
2024-05-10 11:38:39 +0530

    ui changes

--- app/(home)/components/Header.tsx
+++ app/(home)/components/Header.tsx
@@ -4,6 +4,7 @@ import { SiGithub, SiInstagram, SiLinkedin, SiTwitter } from "react-icons/si";
 
 const Header = () => {
   const [menuOpen, setMenuOpen] = useState(false);
+  const [isHovered, setIsHovered] = useState(false);
 
   const toggleMenu = () => {
     setMenuOpen((prevMenuOpen) => !prevMenuOpen);
@@ -14,7 +15,13 @@ const Header = () => {
       <nav className="w-full h-24 shadow-xl bg-black/70 backdrop-blur-70 p-10 rounded-b-3xl ">
         <div className="flex items-center justify-between h-full px-4">
           {/* Site title */}
-          <h1 className="text-3xl font-bold underline underline-offset-8 decoration-green-500 -rotate-3 cursor-pointer">
+          <h1
+            className={`text-3xl font-bold underline underline-offset-8 decoration-green-500 transform ${
+              isHovered ? "-rotate-2" : "-rotate-6"
+            } cursor-pointer transition-transform duration-200 ease-in-out hover:-rotate-2`}
+            onMouseEnter={() => setIsHovered(true)}
+            onMouseLeave={() => setIsHovered(false)}
+          >
             Raunak ✌︎︎
           </h1>
 
@@ -43,7 +50,7 @@ const Header = () => {
           {/* Mobile menu toggle */}
           <div className="sm:hidden cursor-pointer" onClick={toggleMenu}>
             {menuOpen ? (
-              <BsX className="h-8 w-8 text-white" />
+              <BsX className="h-8 w-8 text-white hidden" />
             ) : (
               <BsList className="h-8 w-8 text-white" />
             )}
@@ -52,33 +59,37 @@ const Header = () => {
 
         {/* Mobile menu */}
         {menuOpen && (
-          <div className="fixed top-0 left-0 w-full h-screen bg-black/80 p-10 ease-in-out duration-500">
+          <div className="fixed top-0 left-0 w-screen h-screen bg-black/80 px-20 py-8 ease-in-out duration-500">
             <div className="flex justify-end">
               <BsX
                 className="h-8 w-8 text-white cursor-pointer"
                 onClick={toggleMenu}
               />
             </div>
-            <div className="flex flex-col py-4 gap-3">
+            <div className="flex flex-col py-4 gap-3 items-center">
               {/* Mobile navigation items */}
-              <button className="p-[3px] relative">
+              <button className="p-[3px] relative w-full">
                 <div className="absolute inset-0 bg-gradient-to-r from-indigo-500 to-green-500 rounded-lg" />
                 <div className="px-3 py-2 bg-black rounded-[6px] relative group transition duration-200 text-white hover:bg-transparent">
                   📌 Available for Hire
                 </div>
               </button>
-              <button className="p-[3px] relative">
+              <button className="p-[3px] relative w-full">
                 <div className="absolute inset-0 bg-gradient-to-r from-indigo-500 to-green-500 rounded-lg" />
                 <div className="px-3 py-2 bg-black rounded-[6px] relative group transition duration-200 text-white hover:bg-transparent">
                   Skills
                 </div>
               </button>
-              <button className="p-[3px] relative">
+              <button className="p-[3px] relative w-full">
                 <div className="absolute inset-0 bg-gradient-to-r from-indigo-500 to-green-500 rounded-lg" />
                 <div className="px-3 py-2 bg-black rounded-[6px] relative group transition duration-200 text-white hover:bg-transparent">
                   Projects
                 </div>
               </button>
+
+              <h1 className="text-3xl my-5 font-bold underline underline-offset-8 decoration-green-500 -rotate-3 cursor-pointer">
+                Raunak ✌︎︎
+              </h1>
 
               {/* Social Media Links */}
               <div className="flex gap-10 item-centre pt-6">
