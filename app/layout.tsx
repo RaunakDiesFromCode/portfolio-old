a2109693f74ae4e5b21aea1492b8af207d2a4128
captainTigerYT <raunakmanna43@gmail.com>
2024-05-15 21:18:34 +0530

    ui changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Space_Grotesk } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "next-themes";
+// import CustomCursor from "@/components/Cursor";
+import AnimatedCursor from "react-animated-cursor"
 
 const spaceGrotesque = Space_Grotesk({ subsets: ["latin"] });
 
@@ -35,6 +37,22 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={spaceGrotesque.className}>
+        <AnimatedCursor
+          innerSize={10}
+          outerSize={40}
+          color='0, 0, 0, 0'
+          outerAlpha={0.3}
+          innerScale={2}
+          outerScale={2}
+          trailingSpeed={10}
+          innerStyle={{
+            backgroundColor: 'rgb(0, 255, 255)'
+          }}
+          outerStyle={{
+            // mixBlendMode: 'exclusion',
+            border: '3px solid rgb(0, 255, 255)'
+          }}
+        />
         <ThemeProvider
           attribute="class"
           defaultTheme="dark"
--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,12 @@
-import { withNextVideo } from "next-video/process";
-/** @type {import('next').NextConfig} */
+// @ts-check
+
+/**
+ * @type {import('next').NextConfig}
+ */
 const nextConfig = {
     images: {
         domains: ['i.ibb.co'],
     },
-};
+}
 
-
-
-export default withNextVideo(nextConfig);
+export default nextConfig
